5bd0cae9f59dd14532896c79686b73c83732750e
brkim <fried4chicken@kentech.ac.kr>
2025-06-12 17:19:07 +0900

    update

--- mnist/autoencoder.py
+++ mnist/autoencoder.py
@@ -10,75 +10,35 @@ from torch import optim, nn
 
 
 class Autoencoder(nn.Module):
-    def __init__(self, latent = 10):
+    def __init__(self):
         if not isinstance(latent, int):
             raise TypeError('\'latnet\' should be an integer.')
         if latent < 1:
             raise ValueError('\'latent\' must be positive.')
         super().__init__()
-        self.encoder = nn.ModuleDict()
-        self.decoder = nn.ModuleDict()
 
-        res = 28 * 28
+        self.encoder = nn.Sequential(
+            nn.Sequential(nn.Linear(784, 729), nn.GELU()),
+            nn.Sequential(nn.Linear(729, 243), nn.GELU()),
+            nn.Sequential(nn.Linear(243, 81), nn.GELU()),
+            nn.Sequential(nn.Linear(81, 27), nn.GELU()),
+            nn.Sequential(nn.Linear(27, 9), nn.GELU()),
+            nn.Sequential(nn.Linear(9, 5), nn.Tanh()),
+            )
 
-
-        #encoder
-
-        in_ = res
-        out_ = in_ // 3 + in_ % 3
-        count = 0
-        while out_ > latent:
-            count = count + 1
-            dense = nn.ModuleDict()
-            dense['affine'] = nn.Linear(in_, out_)
-            dense['activation'] = nn.GELU()
-            self.encoder['l'+str(count)] = dense
-
-            in_ = out_
-            out_ = in_ // 3 + in_ % 3
-
-        dense = nn.ModuleDict()
-        dense['affine'] = nn.Linear(in_, latent)
-        dense['activation'] = nn.Tanh()
-        self.encoder['final'] = dense
-
-
-        #decoder
-
-        in_ = latent
-        out_ = in_ * 3
-        count = 0
-        while out_ < res:
-            count = count + 1
-            dense = nn.ModuleDict()
-            dense['affine'] = nn.Linear(in_, out_)
-            dense['activation'] = nn.GELU()
-            self.decoder['l'+str(count)] = dense
-
-            in_ = out_
-            out_ = in_ * 3
-
-        dense = nn.ModuleDict()
-        dense['affine'] = nn.Linear(in_, res)
-        dense['activation'] = nn.Tanh()
-        self.decoder['final'] = dense
-
-
-        #initialization
-        with torch.no_grad():
-            nn.init.xavier_uniform_(self.encoder['final']['affine'].weight)
-            nn.init.xavier_uniform_(self.decoder['final']['affine'].weight)
-
+        self.decoder = nn.Sequential(
+            nn.Sequential(nn.Linear(5, 9), nn.GELU()),
+            nn.Sequential(nn.Linear(9, 27), nn.GELU()),
+            nn.Sequential(nn.Linear(27, 81), nn.GELU()),
+            nn.Sequential(nn.Linear(81, 243), nn.GELU()),
+            nn.Sequential(nn.Linear(243, 729), nn.GELU()),
+            nn.Sequential(nn.Linear(729, 784), nn.Tanh()),
+            )
 
     def forward(self, x):
         x = torch.clone(x)
 
-        for lll in self.encoder.values():
-            x = lll['affine'](x)
-            x = lll['activation'](x)
-        
-        for lll in self.decoder.values():
-            x = lll['affine'](x)
-            x = lll['activation'](x)
+        x = self.encoder(x)
+        x = self.decoder(x)
 
         return x
