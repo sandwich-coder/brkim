5ac529bbdd0bc74d8bfe17196276f70025ea68ab
brkim <fried4chicken@kentech.ac.kr>
2025-06-17 17:14:27 +0900

    update

---
+++ ids/basic.py
@@ -0,0 +1,19 @@
+from copy import deepcopy as copy
+import os, sys
+import time
+import types
+import logging
+import numpy as np
+from scipy import integrate
+from scipy import stats
+from scipy.spatial.distance import pdist, cdist
+import matplotlib as mpl
+from matplotlib import pyplot as pp
+mpl.rcParams['figure.figsize'] = (10, 10)
+mpl.rcParams['axes.titlesize'] = 'xx-small'
+mpl.rcParams['axes.labelsize'] = 'xx-small'
+mpl.rcParams['xtick.labelsize'] = 'xx-small'
+mpl.rcParams['ytick.labelsize'] = 'xx-small'
+mpl.rcParams['legend.fontsize'] = 'xx-small'
+mpl.rcParams['lines.markersize'] = 1
+mpl.rcParams['lines.linewidth'] = 0.5
