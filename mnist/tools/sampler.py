22c44bec0af4254969e01d3ebb5a2beeeb4e92f9
brkim <fried4chicken@kentech.ac.kr>
2025-07-01 17:47:04 +0900

    update

--- mnist/tools/sampler.py
+++ mnist/tools/sampler.py
@@ -8,12 +8,11 @@ class Sampler:
     def __repr__(self):
         return 'sampler'
 
-    def sample(self, A, size = None, axis = 0, replace = False):
+    def sample(self, A, size, axis = 0, replace = False):
         if not isinstance(A, np.ndarray):
             raise TypeError('The array should be a \'numpy.ndarray\'.')
-        if size is not None:
-            if not isinstance(size, int):
-                raise TypeError('The sample size should be an integer.')
+        if not isinstance(size, int):
+            raise TypeError('The sample size should be an integer.')
         if not isinstance(axis, int):
             raise TypeError('\'axis\' should be an integer.')
         if not isinstance(replace, bool):
@@ -21,19 +20,12 @@ class Sampler:
         if not A.ndim >= 1:
             raise ValueError('The array should be at least 1-dimensional.')
         if not -A.ndim <= axis < A.ndim:
-            raise ValueError('\'axis\' must be valid of the array.')
-        if size is not None:
-            if size <= 0:
-                logger.warning('The sample size must be positive.')
-                size = 1
-            if size > A.shape[axis]:
-                logger.warning('The sample size is bigger than the sampled.')
-                size = A.shape[axis]
-        else:
-            if not A.shape[axis] >= 10:
-                size = 1
-            else:
-                size = round(A.shape[axis] / 10)
+            raise ValueError('Not a valid axis.')
+        if size < 1:
+            raise ValueError('The sample size must be positive.')
+        if size > A.shape[axis]:
+            logger.warning('The sample size is bigger than the sampled.')
+            size = A.shape[axis]
         A = A.copy()
 
         index = np.random.choice(
